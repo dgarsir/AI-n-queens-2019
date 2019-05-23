45119c54209bc1cd9a3de052515b05b6983e250c
Neal Rea <nealrea818@gmail.com>
2019-05-23 12:36:07 -0400

    fix getFitness and fitness updating

--- nqueen_ga.py
+++ nqueen_ga.py
@@ -36,12 +36,14 @@ class Individual:
         for row in range(self.size):
             for col in range(self.size):
                 if self.board[row][col] == 1:
+                    #print(row, col)
                     #check column
                     for r in range(self.size):
                         if r != row:
                             if self.board[r][col] == 1:
                                 break
                         if r == self.size-1:
+                            #print('column good')
                             fitness += 1
                     #check row
                     for c in range(self.size):
@@ -49,44 +51,52 @@ class Individual:
                             if self.board[row][c] == 1:
                                 break
                         if c == self.size-1:
+                            #print('row good')
                             fitness += 1
+
+                    #check diagonals
+                    good_lrd = False
+                    good_lld = False
+
                     #check lower right diagonal
-                    r = row+1
-                    c = col+1
+                    r = row
+                    c = col
                     while (r < self.size) & (c < self.size):
-                        if self.board[r][c] == 1:
+                        if (self.board[r][c] == 1) & (r != row) & (c != col):
                             break
                         if (r == self.size-1) | (c == self.size-1):
-                            fitness += 1
+                            good_lrd = True
                         r += 1
                         c += 1
                     #check lower left diagonal
-                    r = row+1
-                    c = col-1
+                    r = row
+                    c = col
                     while (r < self.size) & (c >= 0):
-                        if self.board[r][c] == 1:
+                        if (self.board[r][c] == 1) & (r != row) & (c != col):
                             break
                         if (r == self.size-1) | (c == 0):
-                            fitness += 1
+                            good_lld = True
                         r += 1
                         c -= 1
                     #check upper right diagonal
-                    r = row-1
-                    c = col+1
+                    r = row
+                    c = col
                     while (r >= 0) & (c < self.size):
-                        if self.board[r][c] == 1:
+                        if (self.board[r][c] == 1) & (r != row) & (c != col):
                             break
-                        if (r == 0) | (c == self.size-1):
+                        if ((r == 0) | (c == self.size-1)) & good_lld:
+                            #print('forward diagonal good')
                             fitness += 1
                         r -= 1
                         c += 1
                     #check upper left diagonal
-                    r = row-1
-                    c = col-1
+                    r = row
+                    c = col
                     while (r >= 0) & (c >= 0):
-                        if self.board[r][c] == 1:
+                        if (self.board[r][c] == 1) & (r != row) & (c != col):
                             break
-                        if (r == 0) | (c == 0):
+                        if ((r == 0) | (c == 0)) & good_lrd:
+                            #print('backward diagonal good')
                             fitness += 1
                         r -= 1
                         c -= 1
@@ -107,13 +117,17 @@ class Population:
 
 
 def solution(population):
-    max_iterations = 100
+    max_iterations = 1000
+    max_fitness = 4*population.population[0].size
+    fitness = [0] * population.population[0].size
     new_population = Population(population.size)
 
-    while max_iterations > 0:
-        fitness = []
-        for i in range(population.size):
-            fitness.append(population.population[i].getFitness())
+    fitness = []
+    for i in range(population.size):
+        fitness.append(population.population[i].getFitness())
+
+    #while max_iterations > 0:
+    while (max(fitness) < max_fitness) & (max_iterations > 0):
         total_fitness = np.sum(fitness)
         fitness_probability = np.array(fitness)/total_fitness
 
@@ -131,26 +145,30 @@ def solution(population):
             crossover_point = np.random.randint(new_population.population[0].size)
             offspring = new_population.population[i].board[:crossover_point] + new_population.population[mating_partner].board[crossover_point:]
             
-            #mutate offspring with 0.001 probability
+            # #mutate offspring with 0.001 probability
             # for row in range(new_population.population[0].size):
             #     for col in range(new_population.population[0].size):
             #         if (offspring[row][col] == 1) & (np.random.random() < 0.001):
             #             offspring[row][col] = 0
-            #             if row > 0:
-            #                 offspring[row-1][col] = 1
+            #             if col > 0:
+            #                 offspring[row][col-1] = 1
             #             else:
-            #                 offspring[row+1][col] = 1
+            #                 offspring[row][col+1] = 1
 
             new_population.update(i, offspring)
 
+        fitness = []
+        for i in range(new_population.size):
+            fitness.append(new_population.population[i].getFitness())
+
         max_iterations -= 1
+        
 
     #return best solution
-    fitness = []
-    for i in range(new_population.size):
-        fitness.append(new_population.population[i].getFitness())
-
-    return new_population.population[fitness.index(max(fitness))]
+    if(len(new_population.population) > 0):
+        return new_population.population[fitness.index(max(fitness))]
+    else:
+        return population.population[fitness.index(max(fitness))]
     
 
 pop_size = 10
